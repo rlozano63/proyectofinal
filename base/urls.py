94574901a43727f9cf099e9563bf2da95748a348
ricardo <riquelme.richar@gmail.com>
2017-01-28 17:45:51 -0500

    {problema en guardar cliente}

--- base/urls.py
+++ base/urls.py
@@ -8,13 +8,13 @@ urlpatterns = [
 	url(r'^productos/crear/$', ProductoCreation.as_view(), name="crear_producto"),
 	url(r'^productos/actualizar/(?P<pk>\d+)$', ActualizarProducto.as_view(), name="actualizar_producto"),
 	url(r'^productos/borrar/(?P<pk>\d+)$', BorrarProducto.as_view(), name="borrar_producto"),
-
 	
 
-	url(r'^clientes/$', ListarClientes.as_view(), name="listar_clientes"),
-	url(r'^clientes/crear/$', ClienteCreation.as_view(), name="crear_cliente")
+	url(r'^clientes/$', ListarCliente.as_view(), name="listar_clientes"),
+	url(r'^clientes/crear/$', ClienteCreation.as_view(), name="crear_cliente"),
 	url(r'^clientes/actualizar/(?P<pk>\d+)$', ActualizarCliente.as_view(), name="actualizar_cliente"),
 	url(r'^clientes/borrar/(?P<pk>\d+)$', BorrarCliente.as_view(), name="borrar_cliente"),
+	
 
 
 	url(r'^distribuidores/crear/$', DistribuidorCreation.as_view(), name="crear_distribuidor"),
