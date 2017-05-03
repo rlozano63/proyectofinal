355aa0c19d7d257cb7c9aa28028e163db764f7d6
ricardo <riquelme.richar@gmail.com>
2017-05-03 18:23:51 -0500

    configurando estaticos

--- ForsalesFinal/settings.py
+++ ForsalesFinal/settings.py
@@ -134,7 +134,7 @@ LOGIN_REDIRECT_URL = '/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-#STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
 #MEDIA_ROOT = os.path.join(BASE_DIR,'static')
 
