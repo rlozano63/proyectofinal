417fbe2707ea60bd477bb02e60e0e2f7a7ee2974
Carlos Alonso Turner Benites <carlosturnerbenites@gmail.com>
2018-02-03 10:43:43 -0500

    commit de apoyo

--- clock.py
+++ clock.py
@@ -1,9 +1,15 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from django.core.management import call_command
 
 sched = BlockingScheduler()
 
+@sched.scheduled_job('interval', seconds=10)
+def timed_job():
+    print('This job is run every three minutes.')
+
 @sched.scheduled_job('interval', minutes=3)
 def timed_job():
+    call_command('resetruta')
     print('This job is run every three minutes.')
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
